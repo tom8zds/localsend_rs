a566c49b078ec4fe9da1bc3d1d68f65a567483cb
tom8zds <2407164659@qq.com>
2024-06-01 22:04:24 +0800

    fix

---
+++ build.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:innosetup/innosetup.dart';
+import 'package:version/version.dart';
+
+void main() {
+  InnoSetup(
+    app: InnoSetupApp(
+      name: 'LocalSend-RS',
+      version: Version.parse('0.1.0'),
+      publisher: 'tomzds9@gihub',
+      urls: InnoSetupAppUrls(
+        homeUrl: Uri.parse('https://github.com/tom8zds/localsend_rs'),
+      ),
+    ),
+    files: InnoSetupFiles(
+      executable: File('build/windows/x64/runner/Release/localsend_rs.exe'),
+      location: Directory('build/windows/x64/runner/Release/'),
+    ),
+    name: const InnoSetupName('LocalSend-RS-setup'),
+    location: InnoSetupInstallerDirectory(
+      Directory('build/windows'),
+    ),
+    icon: InnoSetupIcon(
+      File('assets/icon/logo.ico'),
+    ),
+  ).make();
+}
